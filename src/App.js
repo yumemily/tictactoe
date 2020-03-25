dfeaaad7a830cd7948399d6aefc92a08f9b6f263
Emily Yum <emilyyum@gmail.com>
2020-03-25 06:09:27 -0700

    leaderboard and fb login

--- src/App.js
+++ src/App.js
@@ -2,26 +2,123 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board'
+import FacebookLogin from 'react-facebook-login'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      squares: ['','','','','','','','',''],
-      nextPlayer: false // false is x true is O
+    this.state = {
+      squares: ['', '', '', '', '', '', '', '', ''],
+      nextPlayer: false, // false is x true is O
+      history: [],
+      user: '',
+      scores : [],
+      gameOver: false,
+      winner: false
     }
   }
 
-  setParentsState = (obj) =>{
+  setParentsState = (obj) => {
     this.setState(obj)
   }
 
+  showPast = (item, idx) => {
+    this.setState({ squares: item.squares, nextPlayer: item.nextPlayer, history: this.state.history.filter((e, i) => i < idx) })
+  }
 
+  responseFacebook = (response) => {
+    console.log(response)
+    this.setState(
+      { user: response.name }
+    )
+  }
 
+  // call this function when player wins
+  postData = async () => {
+    console.log('here')
+    let data = new URLSearchParams();
+    data.append("player", this.state.user); // data that you want to post (key,value)
+    data.append("score", 3); //figure out how to get time elapsed
+    const url = `http://ftw-highscores.herokuapp.com/tictactoe-dev`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: data.toString(),
+      json: true
+    });
+    console.log('ddd', response)
+  }
+
+  getData = async () => {
+    const url = `http://ftw-highscores.herokuapp.com/tictactoe-dev`;
+    let result = await fetch(url)
+    let data = await result.json()
+    
+    let scores = data.items
+    console.log(scores)
+    this.setState({scores: scores})
+  }
+
+  componentWillMount(nextProps, nextState){
+    {
+      this.getData();
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    if(this.status===true){
+      this.getData();
+      console.log(this.scores)
+      // this.setState({winner: false})
+      this.setState({gameOver: true})
+    }
+  }
+
+  reset = () => {
+    this.getData();
+    this.setState({
+      squares: ['', '', '', '', '', '', '', '', ''],
+      nextPlayer: false, // false is x true is O
+      history: [],
+      gameOver: false,
+      winner: false,
+    });
+  };
+
+  
   render() {
+    if (!this.state.user) {
+      return (
+
+        <FacebookLogin
+          appId="641934443292841"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={this.responseFacebook}
+        />
+      )
+    }
+    
     return (
+      
       <div>
-        <Board { ...this.state} setParentsState={this.setParentsState}/>
+        
+        <h2>User Info: {this.state.user} </h2>
+        <ul>
+          {this.state.history.map((item, idx) => {
+            return (<li><button onClick={() => this.showPast(item, idx)}>Go to move {idx + 1} </button></li>)
+          })}
+        </ul>
+        <h1>Score history</h1>
+        <ol>
+          {this.state.scores.map((item) =>{
+            return (<li>{item.player} {item.score}</li>)
+          })}
+        </ol>
+        <Board {...this.state} setParentsState={this.setParentsState} postData={this.postData} getData={this.getData} />
+      <button  onClick={() => this.reset()}>Reset</button>
       </div>
     );
   }
--- src/components/Board.js
+++ src/components/Board.js
@@ -3,45 +3,52 @@ import Square from './Square'
 
 const calculateWinner = (squares) => {
     const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ];
-    for (let i = 0; i < lines.length; i++){
-      const [a, b, c] = lines[i]; 
-      // create a new array with the same values as each winning combo. i.e. when i = 0 the new array of [a, b, c] is [0, 1, 2]
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-      }
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        // create a new array with the same values as each winning combo. i.e. when i = 0 the new array of [a, b, c] is [0, 1, 2]
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
     return null;
-  };
+};
 
 export default class Board extends React.Component {
+    
     onSquareClicked = (i) => {
         console.log('the box number', i)
         let squareList = this.props.squares.slice(); // copy the whole array into square
-        if (calculateWinner(this.props.squares) || this.props.squares[i]){
+        if (calculateWinner(this.props.squares) || this.props.squares[i]) {
             return;
         }
         squareList[i] = this.props.nextPlayer ? 'O' : 'X'
-        this.props.setParentsState({ squares: squareList, nextPlayer: !this.props.nextPlayer })
+        this.props.setParentsState({ squares: squareList, nextPlayer: !this.props.nextPlayer, history: [... this.props.history, { squares: squareList, nextPlayer: !this.props.nextPlayer }] })
     }
-      
+
+    
+
     render() {
         let status = ''
-    
+
         const winner = calculateWinner(this.props.squares)
         console.log(winner)
-        
+
         if (winner) {
+            this.props.postData();
             status = "Winner: " + winner;
-        } else {
+            // this.props.setParentsState({winner: true})
+            
+        }
+        else {
             status = "Next player: " + (this.props.nextPlayer ? "X" : "O");
         }
 
@@ -63,6 +70,7 @@ export default class Board extends React.Component {
                     <Square value={this.props.squares[7]} onClick={() => this.onSquareClicked(7)} />
                     <Square value={this.props.squares[8]} onClick={() => this.onSquareClicked(8)} />
                 </div>
+                
             </div>
         )
     }
